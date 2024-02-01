3a5970547bff696ea4180f608b590414f5bf400e
khurshid <shamsun.com@gmail.com>
2024-02-01 10:08:50 +0500

    Initial commit

---
+++ lib/pages/image_page.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ImagePage extends StatefulWidget {
+  const ImagePage({super.key});
+
+  @override
+  State<ImagePage> createState() => _ImagePageState();
+}
+
+class _ImagePageState extends State<ImagePage> {
+  String imgNetwork = "https://images.unsplash.com/photo-1493612276216-ee3925520721?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        //child: Image.asset("assets/images/im_sample.png"),
+        // child: Image(
+        //   width: 300,
+        //   height: 300,
+        //   fit: BoxFit.cover,
+        //   image: AssetImage("assets/images/im_sample.png"),
+        // )
+        // child: Image.network(imgNetwork),
+        child: CachedNetworkImage(
+          imageUrl: imgNetwork,
+          placeholder: (context, url) => CircularProgressIndicator(),
+          errorWidget: (context, url, error) => Icon(Icons.error),
+        ),
+      ),
+    );
+  }
+}
